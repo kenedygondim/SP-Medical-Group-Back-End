8168dd00a88c629446e821044f8dfa251ca2a3d0
Kenedy Gondim <g.kenedy@aluno.ifsp.edu.br>
2024-12-15 20:11:32 -0300

    add: Dto criado para receber dados do Front-end

---
+++ SpMedicalGroup/Dto/CriarDisponibilidadeDto.cs
@@ -0,0 +1,10 @@
+﻿namespace SpMedicalGroup.Dto
+{
+    public class CriarDisponibilidadeDto
+    {
+        public required string CpfMedico { get; set; }
+        public required string DataDisp { get; set; }
+        public required string HoraInicio { get; set; }
+        public required string HoraFim { get; set; }
+    }
+}
