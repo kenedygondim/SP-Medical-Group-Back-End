67ce8416318f65443443c1182b7c6404e0dbb9b9
Kenedy Gondim <g.kenedy@aluno.ifsp.edu.br>
2024-12-15 19:39:07 -0300

    add: controller para adicionar disponibilidades

---
+++ SpMedicalGroup/Controllers/DisponibilidadeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SpMedicalGroup.Domains;
+using SpMedicalGroup.Dto;
+using SpMedicalGroup.Models;
+
+namespace SpMedicalGroup.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DisponibilidadeController : ControllerBase
+    {
+        private readonly DisponibilidadeModel disponibilidadeModel = new();
+
+        [HttpPost("adicionar")]
+        public async Task<IActionResult> Adicionar(CriarDisponibilidadeDto novaDisponibilidade)
+        {
+            await disponibilidadeModel.adicionarDisponibilidade(novaDisponibilidade);
+
+            return StatusCode(201);
+        }
+
+    }
+}
