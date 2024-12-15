9fd638fbe50e7b33bfdd6583be7b0458cfd9c0bf
Kenedy Gondim <g.kenedy@aluno.ifsp.edu.br>
2024-12-15 20:12:12 -0300

    add: método de criação de disponibilidade

---
+++ SpMedicalGroup/Models/DisponibilidadeModel.cs
@@ -0,0 +1,27 @@
+﻿using SpMedicalGroup.Contexts;
+using SpMedicalGroup.Domains;
+using SpMedicalGroup.Dto;
+
+namespace SpMedicalGroup.Models
+{
+    public class DisponibilidadeModel
+    {
+        private readonly SpMedicalGroupContext ctx = new();
+
+        public async Task adicionarDisponibilidade(CriarDisponibilidadeDto novaDisponibilidade)
+        {
+            Disponibilidade disponibilidade = new()
+            {
+                CpfMedico = novaDisponibilidade.CpfMedico,
+                DataDisp = novaDisponibilidade.DataDisp,
+                HoraInicio = novaDisponibilidade.HoraInicio,
+                HoraFim = novaDisponibilidade.HoraFim
+            };
+
+            await ctx.Disponibilidades.AddAsync(disponibilidade);
+            await ctx.SaveChangesAsync();
+        }
+
+
+    }
+}
