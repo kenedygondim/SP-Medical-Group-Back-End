1e708e1fb92db91c65feab839cdd7b1b05649d7a
Kenedy Gondim <g.kenedy@aluno.ifsp.edu.br>
2024-12-25 18:15:37 -0300

    add: criação de novos DTOs para recebimento e envio de dados do front…

---
+++ SpMedicalGroup/Dto/ConfirmarConsultaDetalhesDto.cs
@@ -0,0 +1,14 @@
+﻿
+namespace SpMedicalGroup.Dto
+{
+    public class ConfirmarConsultaDetalhesDto
+    {
+        public decimal valorConsulta { get; set; }
+        public string nomeEmpresa { get; set; } = string.Empty;
+        public string municipio { get; set; } = string.Empty;
+        public string bairro { get; set; } = string.Empty;
+        public string logradouro { get; set; } = string.Empty;
+        public string numero { get; set; } = string.Empty;
+        public string complemento { get; set; } = string.Empty;
+    }
+}
