0a1c52cb23c98cb0892374037162df7789809288
Kenedy Gondim <g.kenedy@aluno.ifsp.edu.br>
2024-12-25 18:16:28 -0300

    fix: exclusão de script do BD

--- SpMedicalGroup/Domains/Consulta.cs
+++ SpMedicalGroup/Domains/Consulta.cs
@@ -4,23 +4,6 @@ using System;
 
 namespace SpMedicalGroup.Domains
 {
-
-//    CREATE TABLE tb_consultas(
-//    consulta_id INT IDENTITY(1,1),
-//	disponibilidade_id INT NOT NULL,
-//    especialidade_id INT NOT NULL,
-//	descricao VARCHAR(150) NOT NULL,
-//    situacao VARCHAR(20) NOT NULL, --Criar ENUM na aplicação
-
-//    cpf_paciente CHAR(11) NOT NULL,
-//    is_telemedicina BIT NOT NULL, 
-//--  Definição de chaves primárias e estrangeiras:
-//	PRIMARY KEY(consulta_id),
-//	FOREIGN KEY(disponibilidade_id) REFERENCES tb_disponibilidades(disponibilidade_id),
-//	FOREIGN KEY(especialidade_id) REFERENCES tb_especialidades(especialidade_id),
-//	FOREIGN KEY(cpf_paciente) REFERENCES tb_pacientes(cpf)
-//)
-
     public class Consulta
     {
         public int ConsultaId { get; set; }
