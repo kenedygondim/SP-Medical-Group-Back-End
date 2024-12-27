b98b34664770549857072980b40cc0d2f1b5e30e
Kenedy Gondim <g.kenedy@aluno.ifsp.edu.br>
2024-12-26 21:15:41 -0300

    add: criação de DTO para agendar consulta

---
+++ SpMedicalGroup/Dto/Consulta/AgendarConsultaDto.cs
@@ -0,0 +1,11 @@
+﻿namespace SpMedicalGroup.Dto.Consulta
+{
+    public class AgendarConsultaDto
+    {
+        public required int DisponibilidadeId { get; set; }
+        public required int EspecialidadeId { get; set; }
+        public string Descricao { get; set; } = string.Empty;
+        public string EmailPaciente { get; set; } = string.Empty;
+        public bool IsTelemedicina { get; set; }
+    }
+}
