b3f11375cfef807c864d9e18e39f06fd890779a5
Kenedy Gondim <g.kenedy@aluno.ifsp.edu.br>
2025-01-03 19:26:01 -0300

    add: Dto para cadastro de paciente com opção de adiçaõ de foto de perfil

---
+++ SpMedicalGroup/Dto/Paciente/CadastroPacienteDto.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SpMedicalGroup.Dto.Paciente
+{
+    public class CadastroPacienteDto
+    {
+        public required string NomeCompleto { get; set; }
+        public required string DataNascimento { get; set; } = string.Empty;
+        public required string Rg { get; set; } = string.Empty;
+        public required string Cpf { get; set; } = string.Empty;
+        public required string Email { get; set; } = string.Empty;
+        public required string Senha { get; set; } = string.Empty;
+        public required string Cep { get; set; } = string.Empty;
+        public required string Logradouro { get; set; } = string.Empty;
+        public required string Numero { get; set; } = string.Empty;
+        public required string Bairro { get; set; } = string.Empty;
+        public required string Municipio { get; set; } = string.Empty;
+        public required string Uf { get; set; } = string.Empty;
+        public string Complemento { get; set; } = string.Empty;
+
+        [NotMapped]
+        public IFormFile? FotoPerfilFile { get; set; }
+    }
+}
