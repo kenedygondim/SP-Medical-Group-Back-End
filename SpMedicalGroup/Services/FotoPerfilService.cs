20c9878bdd2932c2c7faa9c67b5261accffd3c13
Kenedy Gondim <g.kenedy@aluno.ifsp.edu.br>
2025-01-03 19:31:43 -0300

    add: método para salvar referência da foto de perfil do usuário no banco de dados

---
+++ SpMedicalGroup/Services/FotoPerfilService.cs
@@ -0,0 +1,17 @@
+﻿using SpMedicalGroup.Contexts;
+using SpMedicalGroup.Models;
+
+namespace SpMedicalGroup.Services
+{
+    public class FotoPerfilService
+    {
+        private readonly SpMedicalGroupContext ctx = new();
+
+        public async Task<FotoPerfil> cadastrarFotoPerfil(FotoPerfil fotoPerfil)
+        {
+            await ctx.FotosPerfil.AddAsync(fotoPerfil);
+            await ctx.SaveChangesAsync();
+            return fotoPerfil;
+        }
+    }
+}
