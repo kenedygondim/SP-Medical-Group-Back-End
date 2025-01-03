145e8478c0cd6ffd8046df9931600a2bfbe41328
Kenedy Gondim <g.kenedy@aluno.ifsp.edu.br>
2025-01-03 19:28:07 -0300

    add: serviço para salvar foto de perfil do usuário em um bucket da AWS

---
+++ SpMedicalGroup/Services/AWS/S3Service.cs
@@ -0,0 +1,47 @@
+﻿using Amazon.S3;
+using Amazon.S3.Transfer;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace SpMedicalGroup.Services.AWS
+{
+    public class S3Service
+    {
+        private readonly string _bucketName = "sp-medical-group";
+        private readonly string _region = "us-east-1"; // Substitua pela região do seu bucket
+
+        private readonly IAmazonS3 _s3Client;
+
+        public S3Service()
+        {
+            // O AWS SDK usará automaticamente o arquivo de credenciais padrão
+            _s3Client = new AmazonS3Client(Amazon.RegionEndpoint.GetBySystemName(_region));
+        }
+
+        public async Task<string> UploadFileAsync(Stream fileStream, string fileName)
+        {
+            try
+            {
+                var transferUtility = new TransferUtility(_s3Client);
+
+                var uploadRequest = new TransferUtilityUploadRequest
+                {
+                    InputStream = fileStream,
+                    Key = fileName,
+                    BucketName = _bucketName,
+                    ContentType = "image/jpeg"
+                };
+
+                await transferUtility.UploadAsync(uploadRequest);
+
+                return $"https://{_bucketName}.s3.{_region}.amazonaws.com/{fileName}";
+            }
+            catch (Exception ex)
+            {
+                // Log ou tratamento de erro
+                throw new Exception("Erro ao fazer upload do arquivo para o S3", ex);
+            }
+        }
+    }
+}
