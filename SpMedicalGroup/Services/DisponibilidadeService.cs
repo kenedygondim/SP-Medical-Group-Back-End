794e075d48e0d48d218c1ed0d0aab381f5901516
Kenedy Gondim <g.kenedy@aluno.ifsp.edu.br>
2025-01-05 18:43:36 -0300

    add: verificações para adição de disponibilidade

--- SpMedicalGroup/Services/DisponibilidadeService.cs
+++ SpMedicalGroup/Services/DisponibilidadeService.cs
@@ -11,12 +11,19 @@ namespace SpMedicalGroup.Services
 
         public async Task<Disponibilidade> AdicionarDisponibilidade(CriarDisponibilidadeDto novaDisponibilidade)
         {
+
+            VerificarValidadeData(novaDisponibilidade);
+
             var cpfMedico =
                 (from tb_usuarios in ctx.Usuarios
                  join tb_medicos in ctx.Medicos
                  on tb_usuarios.UsuarioId equals tb_medicos.UsuarioId
                  where tb_usuarios.Email == novaDisponibilidade.EmailMedico
                  select tb_medicos.Cpf).FirstOrDefault() ?? throw new Exception("Médico não encontrado");
+
+            List<Disponibilidade> disponibilidadesMedico = await ListarDisponibilidadesMedicoPorData(cpfMedico, novaDisponibilidade.DataDisp, false);
+
+            VerificarDisponibilidadeJaPreenchida(disponibilidadesMedico, novaDisponibilidade);
 
             Disponibilidade disponibilidade = new()
             {
@@ -32,18 +39,57 @@ namespace SpMedicalGroup.Services
             return disponibilidade;
         }
 
-        public async Task<List<Disponibilidade>> ListarDisponibilidadesMedicoPorData(string cpf, string data)
+
+        public async Task<List<Disponibilidade>> ListarDisponibilidadesMedicoPorData(string cpf, string data, bool situacaoNull) // situacaoNull = true: retorna apenas as disponibilidades que ainda não foram agendadas (Verificação importante para reutilização de código)
         {   
             var disponibilidadePorData = await
                 (from dis in ctx.Disponibilidades
                  join con in ctx.Consulta on dis.DisponibilidadeId equals con.DisponibilidadeId into consultas
                  from con in consultas.DefaultIfEmpty() // Left join
                  join med in ctx.Medicos on dis.CpfMedico equals med.Cpf
-                 where med.Cpf == cpf && dis.DataDisp == data && con.Situacao == null
+                 where med.Cpf == cpf && dis.DataDisp == data && (!situacaoNull || con.Situacao == null)
                  select dis).ToListAsync();
 
             return disponibilidadePorData;
         }
 
+
+        public void VerificarValidadeData(CriarDisponibilidadeDto novaDisponibilidade)
+        {
+            DateTime data = DateTime.Parse(novaDisponibilidade.DataDisp);
+            TimeSpan horaInicio = TimeSpan.Parse(novaDisponibilidade.HoraInicio);
+            TimeSpan horaFim = TimeSpan.Parse(novaDisponibilidade.HoraFim);
+
+            DateTime dataAtual = DateTime.Today;
+            TimeSpan horaAtual = DateTime.Now.TimeOfDay;
+
+            if (dataAtual > data)
+                throw new Exception("A data da disponibilidade não pode ser anterior à data atual");
+            else if (dataAtual == data && horaAtual > horaInicio)
+                throw new Exception("A hora de início não pode ser anterior à hora atual");
+            else if (horaInicio >= horaFim)
+                throw new Exception("A hora de início deve ser anterior à hora de término");
+            else if (data > dataAtual.AddDays(365))
+                throw new Exception("A data da disponibilidade não pode ser superior a 365 dias da data atual");
+        }
+
+        public void VerificarDisponibilidadeJaPreenchida(List<Disponibilidade> disponibilidadesMedico, CriarDisponibilidadeDto novaDisponibilidade)
+        {
+            TimeSpan horaInicio = TimeSpan.Parse(novaDisponibilidade.HoraInicio);
+            TimeSpan horaFim = TimeSpan.Parse(novaDisponibilidade.HoraFim);
+
+            foreach (Disponibilidade disp in disponibilidadesMedico)
+            {
+                TimeSpan horaInicioDispExistente = TimeSpan.Parse(disp.HoraInicio);
+                TimeSpan horaFimDispExistente = TimeSpan.Parse(disp.HoraFim);
+
+                if ((horaInicio >= horaInicioDispExistente && horaInicio < horaFimDispExistente) ||  // Começa dentro de um intervalo existente
+                    (horaFim > horaInicioDispExistente && horaFim <= horaFimDispExistente) ||        // Termina dentro de um intervalo existente
+                    (horaInicio <= horaInicioDispExistente && horaFim >= horaFimDispExistente))     // Engloba um intervalo existente
+                {
+                    throw new Exception("Já existe uma disponibilidade cadastrada para o médico nesse horário");
+                }
+            }
+        }
     }
 }
