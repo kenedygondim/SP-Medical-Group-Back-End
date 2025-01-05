3955e0ca3597642613fb1f09b7678d33b0aae2ab
Kenedy Gondim <g.kenedy@aluno.ifsp.edu.br>
2025-01-05 18:43:56 -0300

    add: novos endpoints

---
+++ SpMedicalGroup/Controllers/FotoPerfilController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SpMedicalGroup.Dto.FotoPerfil;
+using SpMedicalGroup.Services;
+
+namespace SpMedicalGroup.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FotoPerfilController : ControllerBase
+    {
+        private readonly FotoPerfilService fotoPerfilService = new();
+
+        [HttpPut("AlterarFotoPerfil")]
+        public async Task<IActionResult> AlterarFotoPerfil([FromForm] AlterarFotoPerfilDto novaFotoPerfil)
+        {
+            try
+            {
+                var fotoPerfilAlterada = await fotoPerfilService.AlterarFotoPerfil(novaFotoPerfil);
+                return StatusCode(200, fotoPerfilAlterada);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { mensagem = "Não foi possível alterar foto de perfil. Tente Novamente!", detalhes = ex.Message });
+            }
+        }
+    }
+}
