fe02b6d12ed14a9b4a42c84b2955c12e89b6859f
Kenedy Gondim <g.kenedy@aluno.ifsp.edu.br>
2025-01-06 19:40:08 -0300

    add: tempo de vida dos objetos (scoped, transient, singleton); referência do DbContext

--- SpMedicalGroup/Program.cs
+++ SpMedicalGroup/Program.cs
@@ -1,16 +1,36 @@
 using Serilog;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using SpMedicalGroup.Services;
+using Microsoft.EntityFrameworkCore;
+using SpMedicalGroup.Contexts;
+using SpMedicalGroup.Services.AWS;
+using SpMedicalGroup.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
-
 Log.Logger = new LoggerConfiguration()
     .WriteTo.Console()
     .CreateLogger();
 
-// Add services to the container.
+
+builder.Services.AddDbContext<SpMedicalGroupContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddSingleton<S3Service>(); // Singleton para garantir que a instância seja única
+
+builder.Services.AddScoped<IConsultaService, ConsultaService>();
+builder.Services.AddScoped<IDisponibilidadeService, DisponibilidadeService>();
+builder.Services.AddScoped<IEmpresaService, EmpresaService>();
+builder.Services.AddScoped<IEspecialidadeService, EspecialidadeService>();
+builder.Services.AddScoped<IEnderecoService, EnderecoService>();
+builder.Services.AddScoped<IFotoPerfilService, FotoPerfilService>();
+builder.Services.AddScoped<IUsuarioService, UsuarioService>();
+builder.Services.AddScoped<IPacienteService, PacienteService>();
+builder.Services.AddScoped<IMedicoService, MedicoService>();
+builder.Services.AddScoped<IRoleService, RoleService>();
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -35,19 +55,28 @@ builder.Services.AddAuthentication(options =>
         };
     });
 
+
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll", policy =>
     {
         policy.AllowAnyOrigin()
-              .AllowAnyHeader()
-              .AllowAnyMethod();
+            .AllowAnyHeader()
+            .AllowAnyMethod();
     });
 });
 
 
 
 var app = builder.Build();
+
+app.Use(async (context, next) =>
+{
+    Console.WriteLine($"Request: {context.Request.Method} {context.Request.Path}");
+    await next();
+    Console.WriteLine($"Response: {context.Response.StatusCode}");
+});
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -60,7 +89,7 @@ app.UseHttpsRedirection();
 
 app.UseCors("AllowAll");
 
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
