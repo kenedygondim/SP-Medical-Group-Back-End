e8e8d61daa35e0d39b6d0de21581cd0565731a02
Kenedy Gondim <g.kenedy@aluno.ifsp.edu.br>
2025-01-26 15:42:04 -0300

    add: Dto para criar nova especialidade

---
+++ SpMedicalGroup/Dto/Especialidade/AdicionarEspecialidadeDto.cs
@@ -0,0 +1,8 @@
+﻿namespace SpMedicalGroup.Dto.Especialidade
+{
+    public record AdicionarEspecialidadeDto
+    {
+        public required string NomeEspecialidade { get; set; }
+        public required string DescricaoEspecialidade { get; set; }
+    }
+}
