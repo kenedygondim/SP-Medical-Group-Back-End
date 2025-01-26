1a29727c97795c405471c744a918da67b58a4781
Kenedy Gondim <g.kenedy@aluno.ifsp.edu.br>
2025-01-26 15:42:12 -0300

    chg: alterações secundárias

--- SpMedicalGroup/obj/Debug/net8.0/SpMedicalGroup.AssemblyInfo.cs
+++ SpMedicalGroup/obj/Debug/net8.0/SpMedicalGroup.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SpMedicalGroup")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+074aafc4e3e32378c3a6d6ebbba0723fe2378eb4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b308cf72ba435e3033b92c0febbaada2f4e48c3a")]
 [assembly: System.Reflection.AssemblyProductAttribute("SpMedicalGroup")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SpMedicalGroup")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
