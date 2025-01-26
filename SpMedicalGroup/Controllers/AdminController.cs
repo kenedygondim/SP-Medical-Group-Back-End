20305a8de987e74f52afb56be20472ff8c84b3a1
Kenedy Gondim <g.kenedy@aluno.ifsp.edu.br>
2025-01-26 16:05:10 -0300

    add: admin controller

---
+++ SpMedicalGroup/Controllers/AdminController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace SpMedicalGroup.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+
+        [HttpGet("Acessar")]
+        [Authorize(Roles = "3")]
+        public async Task<IActionResult> Acessar()
+        {
+            return StatusCode(200, "Acesso liberado!");
+        }
+
+    }
+}
